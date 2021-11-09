60e63f75c6c22f0cdab244950373efd46013bcb6
Flashdaniel <flashdaniel0@gmail.com>
2021-11-10 00:04:37 +0100

    update the verify code view

--- bills/urls.py
+++ bills/urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
     path('retrieve/<int:pk>/', views.RetrievesBill.as_view(), name='retrievebill'),
     path('update/<int:pk>/', views.UpdateBill.as_view(), name='updatebill'),
     path('delete/<int:pk>/', views.DestroyBill.as_view(), name='deletebill'),
-    path('payed/', views.ListPayedBill.as_view(), name='listbill'),
-    path('payed/create/', views.CreatePayedBill.as_view(), name='createbill'),
-    path('payed/<int:pk>/', views.RetrievesPayedBill.as_view(), name='retrievebill'),
-    path('payed/update/<int:pk>/',
+    path('paid/', views.ListPayedBill.as_view(), name='listbill'),
+    path('paid/create/', views.CreatePayedBill.as_view(), name='createbill'),
+    path('paid/<int:pk>/', views.RetrievesPayedBill.as_view(), name='retrievebill'),
+    path('paid/update/<int:pk>/',
          views.UpdatePayedBill.as_view(), name='updatebill'),
 ]
